feacdaa543ad5db641872316ed18366de8762467
Anggyluz11 <mary1101sr@gmail.com>
2021-04-26 10:59:40 -0500

    Diseño Pantallas

---
+++ app/src/main/java/hernandez/gewy/iot/RegistroU.java
@@ -0,0 +1,14 @@
+package hernandez.gewy.iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroU extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_u);
+    }
+}
