49b116a6c7973a8fb62f299eed029470e66d93b3
cdhand30 <christian_d_30d@hotmail.com>
2021-05-02 15:08:31 -0500

    Avance

--- app/src/main/java/hernandez/gewy/iot/EncendidoApagado.java
+++ app/src/main/java/hernandez/gewy/iot/EncendidoApagado.java
@@ -2,13 +2,51 @@ package hernandez.gewy.iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.hp.bluetoothjhr.BluetoothJhr;
 
 public class EncendidoApagado extends AppCompatActivity {
-
+    BluetoothJhr bluetoothJhr;
+    Boolean flag;
+    ImageButton ap_enc, closeButt;
+    TextView letter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encendido_apagado);
+        flag=false;
+        ap_enc=findViewById(R.id.apagarEncender);
+        letter=findViewById(R.id.aCTV);
+        closeButt=findViewById(R.id.closeButt);
+        bluetoothJhr = new BluetoothJhr(DispositivoBluetooth.class, this);
+        closeButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(EncendidoApagado.this,MainActivity.class));
+                finish();
+            }
+        });
+        ap_enc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!flag){
+                    ap_enc.setImageResource(R.drawable.abierto);
+                    letter.setText("Abierta");
+                    bluetoothJhr.Tx("on");
+                    flag=true;
+                }else{
+                    ap_enc.setImageResource(R.drawable.cerrado);
+                    letter.setText("Cerrada");
+                    bluetoothJhr.Tx("off");
+                    flag=false;
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/hernandez/gewy/iot/MainActivity.java
+++ app/src/main/java/hernandez/gewy/iot/MainActivity.java
@@ -2,13 +2,41 @@ package hernandez.gewy.iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton acceder;
+    EditText contra;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        acceder=findViewById(R.id.accessButton);
+        contra=findViewById(R.id.contra);
+
+        acceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contra.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Ingrese contraseña",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    String pass=contra.getText().toString();
+                    if(!pass.equals("cdhand30")){
+                        Toast.makeText(getApplicationContext(),"Clave incorrecta",Toast.LENGTH_SHORT).show();
+                    }else{
+                        startActivity(new Intent(MainActivity.this,DispositivoBluetooth.class));
+                        finish();
+                    }
+                }
+            }
+        });
     }
 }
